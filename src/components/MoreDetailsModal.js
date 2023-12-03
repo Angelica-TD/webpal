4a4d2d3401a32c29853b32e7610a5d24b8ff253a
Angie <angelicarlos@icloud.com>
2023-12-03 17:01:22 +1100

    add more details overlay and modal

---
+++ src/components/MoreDetailsModal.js
@@ -0,0 +1,52 @@
+import '../styles/MoreDetailsModal.css'
+
+const MoreDetailsModal = ({ product }) => {
+
+  return (
+
+    <>
+      <div className="modal fade" id="moreDetailsModal">
+        <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <p className="modal-title">Product name</p>
+              <p>Price</p>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <ul>
+                {product && (
+                  <li key={product.id}>
+                    {product.image_urls && product.image_urls.length > 0 && (
+                      <div>
+                        {product.image_urls.map((imageUrl, index) => (
+                          <img
+                            key={index}
+                            src={`http://admin.webpal/uploads/${imageUrl}`}
+                            alt={`${product.name} - ${index}`}
+                            style={{ width: '50px', height: '50px' }}
+                          />
+                        ))}
+                      </div>
+                    )}
+                    {product.name} - ${product.price}
+                  </li>
+                )}
+              </ul>
+              <p>Modal body text goes here.</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary add-to-cart">Add to cart</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </>
+
+  )
+};
+
+export default MoreDetailsModal;
+
--- src/pages/AllProductsPage.js
+++ src/pages/AllProductsPage.js
@@ -1,9 +1,15 @@
 import '../styles/AllProducts.css'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MoreDetailsModal from '../components/MoreDetailsModal';
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState();
+
+  const handleMoreDetailsClick = (product) => {
+    setSelectedProduct(product);
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -14,7 +20,6 @@ const AllProducts = () => {
             const imageResponse = await axios.get(`http://api.webpal/products/${product.product_code}/images`);
 
             if (imageResponse.data && imageResponse.data.length > 0) {
-              // Assuming the API returns an array of image URLs
               return { ...product, image_urls: imageResponse.data.map(image => image.image_name) };
             } else {
               return { ...product, image_urls: [] };
@@ -34,47 +39,48 @@ const AllProducts = () => {
   return (
     <div className='container'>
       <h2>Products</h2>
-      {/* <ul>
-        {products.map((product) => (
-          <li className='product' key={product.id}>
-            {product.image_urls && product.image_urls.length > 0 && (
-              <div>
-                {product.image_urls.map((imageUrl, index) => (
-                  <img
-                    key={index}
-                    src={`http://admin.webpal/uploads/${imageUrl}`}
-                    alt={`${product.name} - ${index}`}
-                    style={{ width: '50px', height: '50px' }}
-                  />
-                ))}
-              </div>
-            )}
-            {product.name} - ${product.price}
-          </li>
-        ))}
-      </ul> */}
       <div className='row row-cols-1 row-cols-md-4'>
         {products.map((product) => (
           <div className='product col' key={product.id}>
             <div className="card h-100">
 
               {product.image_urls && product.image_urls.length > 0 && (
-                <img
-                  src={`http://admin.webpal/uploads/${product.image_urls[0]}`}
-                  className="card-img-top" alt={`${product.name}`} />
+                <div className="card-top">
+                  <img
+                    src={`http://admin.webpal/uploads/${product.image_urls[0]}`}
+                    className="card-img-top" alt={`${product.name}`} />
+
+                  
+                  <div className="more-details-overlay">
+                    <button
+                      type="button"
+                      className="btn btn-light more-details"
+                      data-bs-toggle="modal"
+                      data-bs-target="#moreDetailsModal"
+                      onClick={() => handleMoreDetailsClick(product)}
+                      >
+                      More details
+                    </button>
+                  </div>
+                  
+                </div>
               )}
 
               <div className="card-body d-flex flex-column">
                 <h5 className="card-title">${product.price}</h5>
                 <p className="card-text flex-grow-1">{product.name}</p>
-                
+
                 <button type="button" className="btn btn-primary add-to-cart">Add to cart</button>
 
               </div>
+
+
             </div>
           </div>
         ))}
+        <MoreDetailsModal product={selectedProduct} />
       </div>
+
     </div>
   );
 };
